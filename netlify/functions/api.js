b8044ac654a6f4d01fabe2ff8f0dd98fd047852f
Stany Gregor <your-email@example.com>
2026-02-27 13:09:20 +0530

    Revert serverless handler basePath parameter to standard wrapper implementation

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,4 @@
 import serverless from 'serverless-http';
 import app from '../../server/index.js';
 
-export const handler = serverless(app, {
-    basePath: '/.netlify/functions/api'
-});
+export const handler = serverless(app);
